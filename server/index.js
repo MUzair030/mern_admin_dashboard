00f7ff12aa28789dc0d8f80cc41e492344e51f08
MUzair030 <uzairmushtaq33@gmail.com>
2023-03-28 13:21:41 +0500

    initial setup for server and client sides

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import Routes from "./routes/index.js";
+
+
+// configs
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+// add routes
+Routes(app);
+
+// connect with database
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    app.listen(process.env.SERVER_PORT);
+    console.log(`Server Started on port : ${process.env.SERVER_PORT}`);
+}).catch(err => console.log(`Server failed to start ${err}`));
